34809f7ae8ea6cdb6a3b1989ca25e1a947e97468
Yan Smaliak <ysmaliak@gmail.com>
2024-12-14 13:30:58 +0100

    Increase minimum OS versions

--- Package.swift
+++ Package.swift
@@ -6,10 +6,10 @@ let package = Package(
     name: "RevenueCatUtilities",
     defaultLocalization: "en",
     platforms: [
-        .iOS(.v13),
-        .macOS(.v10_13),
-        .tvOS(.v13),
-        .watchOS(.v6)
+        .iOS(.v15),
+        .macOS(.v12),
+        .tvOS(.v15),
+        .watchOS(.v8)
     ],
     products: [
         .library(
@@ -18,7 +18,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/RevenueCat/purchases-ios.git", from: "5.9.0")
+        .package(url: "https://github.com/RevenueCat/purchases-ios.git", from: "5.14.0")
     ],
     targets: [
         .target(
